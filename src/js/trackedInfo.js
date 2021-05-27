5930915d20ee2bdfffe8fde862c07c31e68dccf3
reigenatk <rma15@illinois.edu>
2021-05-27 16:15:00 -0500

    site looks ok now

--- src/js/trackedInfo.js
+++ src/js/trackedInfo.js
@@ -1,12 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
   const bg = chrome.extension.getBackgroundPage();
+  let parent_div = document.querySelector(".all-videos");
+  // every reload let's query the storage for what to displayt
+  parent_div.innerHTML = "";
+
   Object.keys(bg.videos).forEach(function (url) {
-    if (url !== "data") {
-    }
     console.log(url);
     const div = document.createElement("div");
-    div.textContent = `${bg.videos[url].title}: ${bg.videos[url].views}`;
-    document.body.appendChild(div);
+    div.classList.add("video");
+
+    var videolink = document.createElement("a");
+    videolink.href = url;
+    var thumbnail = document.createElement("img");
+    thumbnail.classList.add("thumbnail-img");
+
+    // id of a youtube video always after the ?v= part
+    let video_id = url.split("?v=")[1].substr(0, 11);
+    thumbnail.src =
+      "https://img.youtube.com/vi/" + video_id + "/maxresdefault.jpg";
+
+    console.log(thumbnail.src);
+
+    videolink.appendChild(thumbnail);
+    div.appendChild(videolink);
+    let text = document.createElement("h1");
+    text.innerHTML = `${bg.videos[url].title}`;
+    div.appendChild(text);
+
+    let viewText = document.createElement("h2");
+    viewText.innerHTML = `Views: ${bg.videos[url].views}`;
+    div.appendChild(viewText);
+    parent_div.appendChild(div);
   });
 
   document.querySelector("#reset").addEventListener("click", () => {
