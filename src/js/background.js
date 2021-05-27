7e2b9ca2ca51bd048309adb5a9f72db6fad51cba
reigenatk <rma15@illinois.edu>
2021-05-27 12:38:40 -0500

    now works with chrome storage api

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,47 @@
 // background script always running
 chrome.runtime.onInstalled.addListener(function () {
+  // if clicked on icon, open up trackedInfo.html in new tab
+  chrome.browserAction.onClicked.addListener(function (tab) {
+    console.log("opening trackedInfo");
+    chrome.tabs.create({
+      url: chrome.extension.getURL("src/html/trackedInfo.html"),
+      selected: true,
+    });
+  });
+
   console.log("ran bg scripts");
   window.videos = {};
+
+  // at the beginning we want to get whatever was left from last session
+  chrome.storage.local.get("data", function (data) {
+    // Notify that we saved.
+    console.log(data);
+    window.videos = data["data"];
+  });
+
+  let update = () => {
+    // here we set window.videos to currentData
+    chrome.storage.local.set({ data: window.videos }, function () {
+      console.log("data saved, window.videos is ");
+      console.log(window.videos);
+    });
+  };
+
+  let eraseLocalStorage = () => {
+    chrome.storage.local.set({ data: {} }, function () {
+      console.log("data erased");
+    });
+    window.videos = {};
+  };
+
+  // eraseLocalStorage();
+
+  let printStorage = () => {
+    chrome.storage.local.get("data", function (data) {
+      // Notify that we saved.
+      console.log(data);
+    });
+  };
 
   chrome.runtime.onMessage.addListener(function (req, sender, sendResponse) {
     if (req.type == "add-view") {
@@ -15,22 +55,31 @@ chrome.runtime.onInstalled.addListener(function () {
           dates: [req.date],
         };
       }
+      update();
 
       sendResponse({ views: window.videos[req.url].views });
     }
+
+    // if bg script detects a reload, run content script to add a view
     if (req.type == "pageRefreshed") {
-      console.log("page refresh");
-      console.log("executing script");
+      console.log("page refresh, executing script");
       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var currTab = tabs[0];
+        var currTab = tabs[0]; // should only be one
         if (currTab) {
-          console.log(currTab.id);
           chrome.tabs.executeScript(currTab.id, {
             runAt: "document_end",
             file: "src/js/content.js",
           });
         }
       });
+    }
+
+    if (req.type == "reset") {
+      eraseLocalStorage();
+    }
+
+    if (req.type == "getData") {
+      printStorage();
     }
   });
 
@@ -40,17 +89,8 @@ chrome.runtime.onInstalled.addListener(function () {
     // or if we visit a yt video from another yt video
     // or if we visit a yt video from a site that isn't a yt video
 
-    // changeInfo === undefined if it is a reload
-    // first expression checks for a reload on yt
-    //
-
-    // if (PerformanceNavigationTiming.type == "reload") {
-    //   console.log("reload detected");
-    //   chrome.runtime.sendMessage({
-    //     type: "reload",
-    //   });
-    // }
-
+    // the last 2 cases are covered here via changeInfo.url
+    // the reloads case is covered in the part above
     console.log(tab.url);
     if (tab.url.indexOf(re) > -1) {
       console.log(changeInfo);
@@ -61,9 +101,6 @@ chrome.runtime.onInstalled.addListener(function () {
           file: "src/js/content.js",
         });
       }
-      //   chrome.tabs.sendMessage(tabId, {
-      //     message: "TabUpdated",
-      //   });
     }
   });
 });
--- src/js/trackedInfo.js
+++ src/js/trackedInfo.js
@@ -1,8 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const bg = chrome.extension.getBackgroundPage();
-    Object.keys(bg.videos).forEach(function (url) {
-        const div = document.createElement('div')
-        div.textContent = `${bg.videos[url].title}: ${bg.videos[url].views}`
-        document.body.appendChild(div);
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const bg = chrome.extension.getBackgroundPage();
+  Object.keys(bg.videos).forEach(function (url) {
+    if (url !== "data") {
+    }
+    console.log(url);
+    const div = document.createElement("div");
+    div.textContent = `${bg.videos[url].title}: ${bg.videos[url].views}`;
+    document.body.appendChild(div);
+  });
+
+  document.querySelector("#reset").addEventListener("click", () => {
+    chrome.runtime.sendMessage({ type: "reset" });
+    console.log("reset signal sent");
+  });
+
+  document.querySelector("#getData").addEventListener("click", () => {
+    chrome.runtime.sendMessage({ type: "getData" });
+  });
+});
